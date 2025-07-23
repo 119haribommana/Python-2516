26ce5033c5d1fa09efba58995cefe50aff37bf96
Komali Bommanaboyina <142759024+119haribommana@users.noreply.github.com>
2025-07-23 20:20:06 +0530

    Add files via upload

---
+++ Python_operations.py
@@ -0,0 +1,104 @@
+# #interpolation
+n="komali"
+age=20
+print(f" My Name is {n} and age is {age}.")
+
+# #Arithmetic operators
+n1=3
+n2=4
+print(n1+n2)
+print(n1-n2)
+print(n1*n2)
+print(n1/n2)
+print(n1%n2)
+print(n1//n2)
+
+# #Compound assignment operators
+n1=5
+n1+=5
+print(n1)
+n1-=5
+print(n1)
+n1*=5
+print(n1)
+n1/=5
+print(n1)
+n1//=5
+print(n1)
+n1%=5
+print(n1)
+
+
+
+
+#Relational Operators
+a=10
+b=20
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+
+#Membership Operators ---> in , not in
+
+# in ---> Checks if the specific number is present in the sequence or not returns True if present returns true otherwise false
+# not in ---> Checks if the specific number is present in the sequence or not returns True 
+
+text="My name is Komali"
+text_p="K" in text
+print(text_p)
+
+text="My name is Komali"
+text_p="K" not in text
+print(text_p)
+
+
+# Identity Operators ---> is, not is 
+# It checks the memory levels
+
+
+num1=10
+num2=20
+print(num1 is num2)
+print(num1 is not num2)
+
+
+Bitwise OPerations
+AND OR NOT
+a=5   # 5---> 0101
+b=3   # 3---> 0011
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(not a)
+print(not b)
+
+
+# left shift <<
+a=3   # 3 ===>0011
+print(3<<2)
+
+
+#Right shift  >>
+a=3   # 3 ===>0011
+print(3>>2)
+
+c = -4 # 1111111111111111
+print(-4>>3) # 1111111111111111
+
+
+
+
+
+
+
+
+
+
+
+
+
+
