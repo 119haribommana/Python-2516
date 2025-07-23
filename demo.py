0cc3305e12bbcac63404665f29e36018ac4bdc84
Komali Bommanaboyina <142759024+119haribommana@users.noreply.github.com>
2025-07-23 17:10:00 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,20 @@
+#print(hello)
+import keyword
+print(keyword.kwlist)
+
+
+# st="komali"
+# print(type(st))
+# print(id(st))
+
+x,y,z="one","two","three"
+print(x,y,z)
+ 
+
+x="komali_bommanaboyina"
+print(type(x))
+print(id(x))
+y="deepu"
+z="chandana"
+print(x,y,z)
+
